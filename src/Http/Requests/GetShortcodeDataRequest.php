7e6826c6877352203bee7e150e07dc28b4da4dd6
Ramin Esmaeilinezhad <rames1363@proton.me>
2023-11-11 01:47:58 +0330

    Initial commit

---
+++ src/Http/Requests/GetShortcodeDataRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Admingate\Shortcode\Http\Requests;
+
+use Admingate\Support\Http\Requests\Request;
+
+class GetShortcodeDataRequest extends Request
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
